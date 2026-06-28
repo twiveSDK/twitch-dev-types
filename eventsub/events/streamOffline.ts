621ce7c68d767a9748e6203184437a852afdacfb
Jacob Foster <jfost3@gmail.com>
2026-06-27 22:25:24 -0500

    fix(StreamOfflineEvent): add missing id property

--- eventsub/events/streamOffline.ts
+++ eventsub/events/streamOffline.ts
@@ -3,4 +3,9 @@ import type {EventBroadcasterInfo} from "./common";
 /**
  * @see https://dev.twitch.tv/docs/eventsub/eventsub-reference/#stream-offline-event
  */
-export interface StreamOfflineEvent extends EventBroadcasterInfo {}
+export interface StreamOfflineEvent extends EventBroadcasterInfo {
+    /**
+     * The id of the stream.
+     */
+    id: string;
+}
